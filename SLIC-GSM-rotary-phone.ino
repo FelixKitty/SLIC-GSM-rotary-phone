5dc121ce22deb92483adbff96e7783b60431fb7d
FelixKitty <62228422+FelixKitty@users.noreply.github.com>
2020-08-10 16:01:25 +1000

    Update SLIC-GSM-rotary-phone.ino

--- SLIC-GSM-rotary-phone.ino
+++ SLIC-GSM-rotary-phone.ino
@@ -7,24 +7,44 @@
 //  For Arduino, QCX601 SLIC board and SIM900 or SIM800 breakout board    //
 //                                                                        // 
 ////////////////////////////////////////////////////////////////////////////
+#include "Adafruit_FONA.h"
 
-#define GSM_MODULE   //(fona libaray "getNetworkStatus" ?)
-//#define SLIC_TEST
+#define SIMCOM_7000 // SIM7000A/C/E/G
 
-#if defined(GSM_MODULE)  //#if defined(getNetworkStatus)
-//#include "SIM900.h"
+#define SLIC_TEST
+
+#define FONA_PWRKEY 6
+#define FONA_RST 7
+
+#define FONA_TX 10 // Microcontroller RX
+#define FONA_RX 11 // Microcontroller TX
+
+// this is a large buffer for replies
+char replybuffer[255];
+
 #include <SoftwareSerial.h>
-//#include "call.h"
-#endif
+SoftwareSerial fonaSS = SoftwareSerial(FONA_TX, FONA_RX);
 
-// states of state machine    EDIT TO: state = getCallStatus 
-#define IDLE_WAIT 1                 //  0 IDLE_WAIT
-#define RINGING 2                   //  3 RINGING
-#define ACTIVE_CALL 3                // 4 ACTIVE_CALL
-#define GETTING_NUMBER 4             // 2 unknown
+SoftwareSerial *fonaSerial = &fonaSS;
+
+// Use this one for LTE CAT-M/NB-IoT modules (like SIM7000)
+// Notice how we don't include the reset pin because it's reserved for emergencies on the LTE module!
+if defined(SIMCOM_7000) || defined(SIMCOM_7500)
+  Adafruit_FONA_LTE fona = Adafruit_FONA_LTE();
+#end if
+
+uint8_t readline(char *buff, uint8_t maxbuff, uint16_t timeout = 0);
+uint8_t type;
+char imei[16] = {0}; // MUST use a 16 character buffer for IMEI!
 
 
-//#define rflPin 6        // RFL pin to QCX601 pin J1-1, reverse signal (LOW)
+#define IDLE_WAIT 1                
+#define RINGING 2                  
+#define ACTIVE_CALL 3               
+#define GETTING_NUMBER 4             
+
+
+
 #define hzPin 7         // 25Hz pin to QCX601 pin J1-2 (25Hz signal for ringing) _-_-_-_-_
 #define rcPin 8         // RC pin to QCX601 pin J1-3, ring control, HIGH when ringing --___--___--___
 #define shkPin 9        // switch hook pin from QCX601 pin J1-4
@@ -68,13 +88,11 @@ byte pulses = 0;
 byte digit = 0;
 String number = "";
 byte digits = 0;            // the number of digits we have collected
-char numArray[11];
-byte gsmStatus;             //byte lteStatus
+char numArray[10];
+byte lteStatus;            
 byte state;
 
-#if defined(GSM_MODULE)
-CallGSM call;
-#endif
+
 
 ////////////////////////////////////////////////////////////////////////////
 // 
@@ -82,24 +100,111 @@ CallGSM call;
 //
 
 void setup() {
+ 
+  pinMode(FONA_RST, OUTPUT);
+  digitalWrite(FONA_RST, HIGH); // Default state
+
+  pinMode(FONA_PWRKEY, OUTPUT);
+
+  // Turn on the module by pulsing PWRKEY low for a little bit
+  // This amount of time depends on the specific module that's used
+  powerOn(); // See function definition at the very end of the sketch
+
+  Serial.begin(9600);
+  Serial.println(F("FONA basic test"));
+  Serial.println(F("Initializing....(May take several seconds)"));
+
+  // SIM7000 takes about 3s to turn on but SIM7500 takes about 15s
+  // Press reset button if the module is still turning on and the board doesn't find it.
+  // When the module is on it should communicate right after pressing reset
+  
+  fonaSS.begin(115200); // Default SIM7000 shield baud rate
+
+  Serial.println(F("Configuring to 9600 baud"));
+  fonaSS.println("AT+IPR=9600"); // Set baud rate
+  delay(100); // Short pause to let the command run
+  fonaSS.begin(9600);
+  if (! fona.begin(fonaSS)) {
+    Serial.println(F("Couldn't find FONA"));
+    while (1); // Don't proceed if it couldn't find the device
+  }
+  
+   type = fona.type();
+  Serial.println(F("FONA is OK"));
+  Serial.print(F("Found "));
+  switch (type) {
+    case SIM5320A:
+      Serial.println(F("SIM5320A (American)")); break;
+    case SIM5320E:
+      Serial.println(F("SIM5320E (European)")); break;
+    case SIM7000A:
+      Serial.println(F("SIM7000A (American)")); break;
+    case SIM7000C:
+      Serial.println(F("SIM7000C (Chinese)")); break;
+    case SIM7000E:
+      Serial.println(F("SIM7000E (European)")); break;
+    case SIM7000G:
+      Serial.println(F("SIM7000G (Global)")); break;
+    case SIM7500A:
+      Serial.println(F("SIM7500A (American)")); break;
+    case SIM7500E:
+      Serial.println(F("SIM7500E (European)")); break;
+    default:
+      Serial.println(F("???")); break;
+  }
+
+  // Print module IMEI number.
+  uint8_t imeiLen = fona.getIMEI(imei);
+  if (imeiLen > 0) {
+    Serial.print("Module IMEI: "); Serial.println(imei);
+  }
+  
+    // Set modem to full functionality
+  fona.setFunctionality(1); // AT+CFUN=1
+
+  // Configure a GPRS APN, username, and password.
+  // You might need to do this to access your network's GPRS/data
+  // network.  Contact your provider for the exact APN, username,
+  // and password values.  Username and password are optional and
+  // can be removed, but APN is required.
+  //fona.setNetworkSettings(F("your APN"), F("your username"), F("your password"));
+  //fona.setNetworkSettings(F("m2m.com.attz")); // For AT&T IoT SIM card
+  //fona.setNetworkSettings(F("telstra.internet")); // For Telstra (Australia) SIM card - CAT-M1 (Band 28)
+  fona.setNetworkSettings(F("hologram")); // For Hologram SIM card
+
+  // Optionally configure HTTP gets to follow redirects over SSL.
+  // Default is not to follow SSL redirects, however if you uncomment
+  // the following line then redirects over SSL will be followed.
+  //fona.setHTTPSRedirect(true);
+
+  /*
+  // Other examples of some things you can set:
+  fona.setPreferredMode(38); // Use LTE only, not 2G
+  fona.setPreferredLTEMode(1); // Use LTE CAT-M only, not NB-IoT
+  fona.setOperatingBand("CAT-M", 12); // AT&T uses band 12
+//  fona.setOperatingBand("CAT-M", 13); // Verizon uses band 13
+  fona.enableRTC(true);
+  
+  fona.enableSleepMode(true);
+  fona.set_eDRX(1, 4, "0010");
+  fona.enablePSM(true);
+  // Set the network status LED blinking pattern while connected to a network (see AT+SLEDS command)
+  fona.setNetLED(true, 2, 64, 3000); // on/off, mode, timer_on, timer_off
+  fona.setNetLED(false); // Disable network status LED
+  */
+}
+  
+  
   pinMode(shkPin, INPUT);
   pinMode(hzPin, OUTPUT);
   pinMode(rcPin, OUTPUT);
-  pinMode(rflPin, OUTPUT);
-  digitalWrite(rflPin, 0);
+  digitalWrite(hzPin, 0);
   #if defined(SLIC_TEST)
   pinMode(ringTestPin, INPUT_PULLUP);
   #endif
 
-  Serial.begin(9600); // start serial for debug monitor
   Serial.println("\nHello, I am your telephone.");
-  #if defined(GSM_MODULE)
-  if (gsm.begin(2400)) {
-    Serial.println("\nstatus=READY");
-  } else {
-    Serial.println("\nstatus=IDLE");
-  }
-  #endif
+
   state = IDLE_WAIT;
 }
 
@@ -133,9 +238,9 @@ void loop() {
 
   if ((unsigned long)(currentMillis - statusPreviousMillis) >= statusCheckInterval) {
     // Time to check status of GSM board
-    #if defined(GSM_MODULE)         //if Network status = 1
-    if (!digitalRead(rcPin) && (state != GETTING_NUMBER)) { // or software serial will interfere with ringing and dialing
-      gsmStatus = call.CallStatus();   //lteStatus = getcallstauts
+    #if defined(SIMCOM_7000)        
+    if (!digitalRead(rcPin) && (state != GETTING_NUMBER) { // or software serial will interfere with ringing and dialing
+      gsmStatus = call.CallStatus();   //lteStatus = getCallStatus();
     }
     #endif
     #if defined(SLIC_TEST)
@@ -147,19 +252,19 @@ void loop() {
 
   ////////////////////////////////////////////////////////////////////////////
   // 
-  //  STATE IDLE_WAIT
+  //  STATE IDLE_WAIT         ///READY
   //
   if (state == IDLE_WAIT) {
     // wait for incoming call or picking up reciever
-    #if defined(GSM_MODULE)
-    if (gsmStatus == CALL_INCOM_VOICE) {
+    #if defined(SIMCOM_7000)
+    if (gsmStatus == CALL_INCOM_VOICE) {  //lteStatus == RINGING
       Serial.println("Incoming call. Ringing.");
-      state = RINGING;
+      state = RINGING;  
     }
     if (gsmStatus == CALL_ACTIVE_VOICE) {
       // this should not happen
       // hanging up must have failed, try again
-      call.HangUp();
+      HangUp();
       delay(2000); 
     }
     #endif
@@ -205,12 +310,12 @@ void loop() {
       digitalWrite(hzPin, 0);
       // tell GSM board to pick up
       Serial.println("Picking up. Call initiated.");
-      #if defined(GSM_MODULE)
-      call.PickUp();
+      #if defined(SIMCOM_7000)
+      pickUp();
       #endif   
       state = ACTIVE_CALL; 
     }
-    #if defined(GSM_MODULE)
+    #if defined(SIMCOM_7000)
     if (gsmStatus != CALL_INCOM_VOICE) {
       digitalWrite(rcPin, 0); // stop ringing
       digitalWrite(hzPin, 0);
@@ -236,14 +341,14 @@ void loop() {
     if ((shkState == LOW) && ((unsigned long)(currentMillis - lastShkFall) >= tHup)) {
       // tell GSM board to disconnect call, flush everything, then go idle
       Serial.println("Hanging up. Going idle.");
-      #if defined(GSM_MODULE)
-      call.HangUp();
+      #if defined(SIMCOM_7000)
+      hangUp();
       #endif
       flushNumber();
       delay(1000); // wait a sec before going to next state to make sure GSM module keeps up
       state = IDLE_WAIT;
     }
-    #if defined(GSM_MODULE)
+    #if defined(SIMCOM_7000)
     if (gsmStatus == CALL_NONE) {
       Serial.println("Call disconnected. Going idle.");
       flushNumber();
@@ -293,9 +398,9 @@ void loop() {
       if (digits == 1) getFave();
       Serial.print("Number complete, calling: ");
       Serial.println(number);
-      number.toCharArray(numArray, 11);
-      #if defined(GSM_MODULE)
-      call.Call(numArray);
+      number.toCharArray(numArray, 10);
+      #if defined(SIMCOM_7000)
+      callPhone(numArray);
       #endif
       delay(1000); // wait a sec before going to next state to make sure GSM module keeps up
       state = ACTIVE_CALL;
